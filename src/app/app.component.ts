f80f57f55eb668eed2dce19fe409fba4ae8918aa
Quentin Crida <qcrida@gmail.com>
2020-08-07 10:23:06 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+// // EXPERIMENTS
+// export class AppComponent {
+//   name: string = "Quentin";
+//   age: number = 21;
+
+// constructor() {
+//   this.changeName('Hester');
+ 
+//   }
+//   changeName(name: string):void {
+//     this.name = name;
+//   }
+// }
+
+export class AppComponent {
+  
+}
