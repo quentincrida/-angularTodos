8cb382ef670e1912d3c0f1ad4eb7374710a2af18
Quentin Crida <qcrida@gmail.com>
2020-08-07 14:06:58 +0100

    header added

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -21,7 +21,9 @@ export class TodosComponent implements OnInit {
     });
   }
   deleteTodo(todo:Todo){
+    //Remove from UI 
     this.todos = this.todos.filter(t => t.id !== todo.id);
+  // Remove from Server
     this.todoService.deleteTodo(todo).subscribe();    
   } 
 
